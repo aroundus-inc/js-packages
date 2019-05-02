da610881f1b090cbe392cfd53a0d0160ffae0e01
Taehyun Kim <kgyoo8232@gmail.com>
2019-05-02 15:10:41 +0900

    parserOptions for typescript

--- packages/eslint-config-aroundus/typescript.js
+++ packages/eslint-config-aroundus/typescript.js
@@ -1,5 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
   plugins: ['@typescript-eslint'],
   extends: [
     require.resolve('./prettier'),
